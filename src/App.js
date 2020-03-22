c44dd9065ca966b3e67080aafcd656dc5fe79e8d
joseiciano <icianojose12345@aol.com>
2020-03-21 23:12:29 -0400

    deleted useless files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import "./App.css";
 
 class App extends Component {
   render() {
