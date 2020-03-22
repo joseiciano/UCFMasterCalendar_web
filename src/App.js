9a0dca17b529aba92f9140f42e0dbd6d966c72dd
joseiciano <icianojose12345@aol.com>
2020-03-21 23:09:32 -0400

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
@@ -10,6 +12,9 @@ class App extends Component {
     return (
       <Router>
         <Route exact path="/" component={Main} />
+        <Route exact path="/main" component={Main} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
       </Router>
     );
   }
