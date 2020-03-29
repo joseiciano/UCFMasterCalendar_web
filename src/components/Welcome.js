7c6c432e281d6107aee2bb33540fe07c93234dd7
Sarah71144 <54606672+Sarah71144@users.noreply.github.com>
2020-03-29 18:57:56 -0400

    resizing, formatting

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,27 +8,36 @@ export default class Welcome extends Component {
     render() {
         return (
             <div>
-                <Row responsive>
-                    <Col sm={{ span: 8, offset: 0 }} responsive>
+                    <Col lg={{ span: 10, offset: 1 }} responsive>
                         <Card style={styles.wCard} responsive >
                             <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet"/>
-                            <Card.Text style={styles.headerText} responsive>
-                               
+                            
+                <Row>
+                            
+                            <Col sm={{ span: 5, offset: 1 }}>
+                                <Card.Text style={styles.headerText} responsive>
+                                    <br/>
                                 <b> Something for
-                                  <br/>  Everyone </b>
-                             </Card.Text>
+                                <br />  Everyone </b>
+
+                            </Card.Text>
+
                             <Card.Text style={styles.bodyText} responsive>
                                 Just bring an open mind and an insatiable desire <br /> 
                                 to learn, and we'll take care of the rest. Create <br />
                                 something, learn new skills, and have fun with <br />
                                 friends old and new.
                              </Card.Text>
-                            <Card.Img src={heartPic} style={styles.hearty} responsive />
 
+                        </Col>
+
+                        <Col sm={{ span: 5, offset: 1 }}>
+                            <Card.Img src={heartPic} style={styles.hearty} fluid />
+                        </Col>
+                        </Row>
             </Card>
             </Col>
             
-            </Row>
             </div>
         )
     }
@@ -40,36 +49,28 @@ const styles = {
             backgroundColor:  "#D0F5F7",
             color: "#1C8D9B",
             fontSize: "250%",
-            height: "50%",
-            left: "4%",
-            width: "144%",
             borderRadius: "50px 50px 50px 50px",
             borderStyle: "none",
             boxShadow: "1.5px 1.5px #E6E6E6",
         },
                         
     hearty: {
-        position: "relative",
-        width: "40%",
-        height: "98.5%",
-        right: "-60%",
+        height: "100%",
+        maxWidth: "100%",
         borderRadius: "50px 50px 50px 50px",
-        top: "-85%"
     },
 
     bodyText: {
+        top: "-10%",
         position: "relative",
         fontFamily: "Quicksand",
-        marginLeft: "10%",
-        fontSize: "40%",
-        top: "7%",
+        fontSize: "medium",
     },
 
     headerText: {
+        top: "-4%",
         position: "relative",
         fontFamily: "Arial",
-        marginLeft: "10%",
-        fontSize: "75%",
-        top: "10%"
+        fontSize: "xx-large",
     }
 };
