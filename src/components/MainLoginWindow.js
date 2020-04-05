4c9942c3add90188e53184b00c3ff4906a0925bc
bambu <bambu@DESKTOP-E3G4EOS.localdomain>
2020-04-05 01:42:09 -0700

    WIP: form fields and login box adjusted

--- src/components/MainLoginWindow.js
+++ src/components/MainLoginWindow.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Card, Image, Row, Col } from 'react-bootstrap';
+import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import dude from "../icons/dude.png"
 
 
@@ -11,13 +12,28 @@ export default class Welcome extends Component {
     {
         return (
             <div>
-                <Container lg={{span: 10, offset: 1}} responsive>>
+                <Container>
                     <Card style={Styles.mainWindow} responsive >  
                         <Row>
-                        <Col sm={{ span: 5, offset: 1 }}>
+                        <Col xs={6} md={5}>
+                            <Card.Text style={Styles.headerText} responsive>
+                                <b> Knightro </b>
+                            </Card.Text>
+                            <Card.Text style={Styles.formFields} responsive> 
+                                <Form>
+                                    <Form.Group controlId="formGroupEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                    </Form.Group>
+                                    <Form.Group controlId="formGroupPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                    </Form.Group>
+                                </Form>
+                            </Card.Text>
                         </Col>
-                        <Col sm={{ span: 5, offset: 1 }}>
-                        <Card.Img src={dude} style={Styles.dude} fluid />
+                        <Col>
+                            <Card.Img src={dude} style={Styles.dude} fluid />
                         </Col>
                         </Row>
                     </Card>
@@ -32,7 +48,7 @@ export default class Welcome extends Component {
 
 const Styles = {
     mainWindow: {
-        position: "realtive",
+        position: "relative",
         marginTop: 50,
         marginBottom: 50,
         borderRadius: "50px 50px 50px 50px",
@@ -45,5 +61,21 @@ const Styles = {
         maxWidth: "100%",
         borderRadius: "50px 50px 50px 50px"
     },
+
+    headerText: {
+        top: "20%",
+        left: "10%",
+        position: "relative",
+        fontFamily: "Arial",
+        fontSize: "xxx-large",
+    },
+
+    formFields: {
+        top: "20%",
+        left: "10%",
+        position: "relative",
+        fontFamily: "Arial",
+        
+    }
 
 };
