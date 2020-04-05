935d3abe1e8cb3987b3fc812b310b3f675a06676
Brian <brianbaso@gmail.com>
2020-04-05 16:49:31 -0400

    add configto project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUTkhZBvUiA9lIM00LbFaMijNMI7Vzk1o",
+  authDomain: "ucf-master-calendar.firebaseapp.com",
+  databaseURL: "https://ucf-master-calendar.firebaseio.com",
+  projectId: "ucf-master-calendar",
+  storageBucket: "ucf-master-calendar.appspot.com",
+  messagingSenderId: "641833784596",
+  appId: "1:641833784596:web:2b32efb77a94a470be5e01",
+  measurementId: "G-486GH9KB3W"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
