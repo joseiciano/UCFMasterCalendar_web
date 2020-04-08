38fc046a359b1c87f83d2047a6f1885b4a7f20f8
Sarah71144 <54606672+Sarah71144@users.noreply.github.com>
2020-04-08 14:23:58 -0400

    Added functionality to all buttons in Nav except clubs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,8 @@ import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
 
+// TODO: add onclick for clubs
+
 export default class MainNavbar extends Component {
 
     render(){
@@ -15,28 +17,28 @@ export default class MainNavbar extends Component {
                         {/*? Possibily will have to change home to Main because that is the component name?*/}
                         {/*? OR WILL have to put #url! that seems like the best option right now*/}
                         <Col sm={{span: 3, offset: 0}}>
-                            <Navbar.Brand style={styles.knightro} > Knightro </Navbar.Brand>
+                            <Navbar.Brand style={styles.knightro} onClick={navigateHome} > Knightro </Navbar.Brand>
                         </Col>
 
                         <Row >
                             <Nav className="NavOptions">
                            
                                 <Col sm={{ span: 2, offset: 8 }}>
-                                        <Nav.Link href="#Main" style={styles.center}> Home </Nav.Link>
+                                    <Nav.Link href="#Main" style={styles.center} onClick={navigateHome}> Home </Nav.Link>
                                 </Col>
                                 
                                 <Col lg={{ span: 7, offset: 0 }}>
-                                        <Nav.Link href="#Clubs" style={styles.center}> Clubs </Nav.Link>
+                                    <Nav.Link href="#Clubs" style={styles.center} > Clubs </Nav.Link>
                                 </Col>
                                 
 
                                 <Col lg={{ span: 3, offset: 6}}>
 
-                                    <Nav.Link href="#LogIn"> Log in </Nav.Link>
+                                    <Nav.Link href="#LogIn" onClick={navigateLogin}> Log in </Nav.Link>
                                 </Col>
                                 
                                 <Col lg={{ span: 4, offset: 0 }}>
-                                    <Nav.Link href="#SignUp"> Sign Up </Nav.Link>
+                                    <Nav.Link href="#SignUp" onClick={navigateSignup}> Sign Up </Nav.Link>
 
                                 </Col>
                             </Nav>
@@ -52,6 +54,20 @@ export default class MainNavbar extends Component {
     }
     
 }
+
+const navigateHome = () => {
+    window.location.href = "/Main";
+};
+
+const navigateLogin = () => {
+    window.location.href = "/login";
+};
+
+const navigateSignup = () => {
+    window.location.href = "/register";
+};
+
+
 
 const styles = {
     // href is that little badge in the left hand corner that tells you the hyperlink address
